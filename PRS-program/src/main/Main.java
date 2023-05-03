843d715a8151fbbe743902ef70fd56e5b2b89a0f
MilanVlaski <milan.vlaski321@gmail.com>
2023-05-03 10:41:13 +0200

    napisao ParallelMergeSort klasu koja poziva MergeSort metode

--- PRS-program/src/main/Main.java
+++ PRS-program/src/main/Main.java
@@ -5,6 +5,7 @@ import algorithams.InsertionSort;
 import algorithams.MergeSort;
 import algorithams.QuickSort;
 import algorithams.SelectionSort;
+import parallel.ParallelMergeSort;
 
 public class Main {
 	public static void main(String[] args) {
@@ -17,10 +18,18 @@ public class Main {
 //		  InsertionSort.insertionSort(niz);
 //		  SelectionSort.selectionSort(niz);
 //		  BubbleSort.bubbleSort(niz);
+//		  QuickSort.sort(niz, 0, niz.length-1);	
 		  startTime = System.currentTimeMillis();
 		  MergeSort.mergeSort(niz, 0, niz.length-1);
 		  endTime = System.currentTimeMillis();
-//		  QuickSort.sort(niz, 0, niz.length-1);	
+		  ispis(niz);
+		  System.out.println("\nVrijeme izvrsenja = " + (endTime - startTime) + "ms");
+		  
+		  niz = new int[] {3,7,0,2,44,-23,8,1,14,2,-8,9,5,6,4,3,1};
+		  ispis(niz);
+		  startTime = System.currentTimeMillis();
+		  ParallelMergeSort.parallelMergeSort(niz, 0, niz.length - 1);
+		  endTime = System.currentTimeMillis();
 		  ispis(niz);
 		  System.out.println("\nVrijeme izvrsenja = " + (endTime - startTime) + "ms");
 	}
---
+++ PRS-program/src/parallel/ParallelMergeSort.java
@@ -0,0 +1,46 @@
+package parallel;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
+
+import algorithams.MergeSort;
+
+public class ParallelMergeSort {
+
+    public static void parallelMergeSort(int[] niz, int lijevi, int desni) {
+        SortTask task = new SortTask(niz, lijevi, desni);
+        ForkJoinPool pool = new ForkJoinPool();
+        pool.invoke(task);
+    }
+	
+	public static class SortTask extends RecursiveAction {
+
+		private int[] niz;
+		private int lijevi;
+		private int desni;
+
+		public SortTask(int[] niz, int lijevi, int desni) {
+			super();
+			this.niz = niz;
+			this.lijevi = lijevi;
+			this.desni = desni;
+		}
+		
+		@Override
+		protected void compute() {
+			
+			if(lijevi < desni) {
+				
+				 int srednji = (lijevi + desni) / 2;
+				 
+				 SortTask prvaPolovina = new SortTask(niz, lijevi, srednji);
+				 SortTask drugaPolovina = new SortTask(niz, srednji + 1, desni);
+				 
+				 invokeAll(prvaPolovina, drugaPolovina);
+				 
+				 MergeSort.merge(niz, lijevi, srednji, desni);
+			}
+		}
+		
+	}
+}
