0b10f0e43b5a635a070d442096e1e4ff94774c79
MilanVlaski <milan.vlaski321@gmail.com>
2023-05-03 10:04:06 +0200

    dodao vrijeme izvrsenja

--- PRS-program/src/main/Main.java
+++ PRS-program/src/main/Main.java
@@ -9,15 +9,20 @@ import algorithams.SelectionSort;
 public class Main {
 	public static void main(String[] args) {
 	
+		long startTime;
+		long endTime;
+		
 		int[] niz={3,7,0,2,44,-23,8,1,14,2,-8,9,5,6,4,3,1};
 		  ispis(niz);
 //		  InsertionSort.insertionSort(niz);
 //		  SelectionSort.selectionSort(niz);
 //		  BubbleSort.bubbleSort(niz);
-//		  MergeSort.mergeSort(niz, 0, niz.length-1);
-		  QuickSort.sort(niz, 0, niz.length-1);	
+		  startTime = System.currentTimeMillis();
+		  MergeSort.mergeSort(niz, 0, niz.length-1);
+		  endTime = System.currentTimeMillis();
+//		  QuickSort.sort(niz, 0, niz.length-1);	
 		  ispis(niz);
-		
+		  System.out.println("\nVrijeme izvrsenja = " + (endTime - startTime) + "ms");
 	}
 	
 	
