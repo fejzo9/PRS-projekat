34d2820bd15b7679297bd2fab36a70f8e6902293
MilanVlaski <milan.vlaski321@gmail.com>
2023-06-17 12:30:26 +0200

    main doradjen

--- PRS-program/src/main/Main.java
+++ PRS-program/src/main/Main.java
@@ -1,15 +1,11 @@
 package main;
 
 import algorithms.BubbleSort;
-import algorithms.InsertionSort;
 import algorithms.Sort;
 import model.SortModel;
 
 public class Main {
 	public static void main(String[] args) {
-	
-		long startTime;
-		long endTime;
 		
 		SortModel sortModel = new SortModel();
 		
@@ -20,35 +16,6 @@ public class Main {
 		Double[] nizDouble = sortModel.getRandomDoubles(50);
 		testiraj(nizDouble, bubbleSort);
 		
-//		System.out.println("SEKVENCIJALNI BUBBLE SORT SA DOUBLESIMA");
-//		Double[] nizDub = sortModel.getRandomDoubles(50);
-//		ispis(nizDub);
-//		startTime = System.currentTimeMillis();
-//		bubbleSort.sort(nizDub, 0, nizDub.length - 1);
-//		endTime = System.currentTimeMillis();
-//		ispis(nizDub);
-//		System.out.println("\nVrijeme izvrsenja = " + (endTime - startTime) + "ms\n");
-		
-//		System.out.println("SEKVENCIJALNI INSERTION SORT SA INTEGERIMA");
-//		nizInt = sortModel.getRandomInts(50);
-//		ispis(nizInt);
-//		startTime = System.currentTimeMillis();
-//		InsertionSort insertionSort = new InsertionSort();
-//		insertionSort.sort(nizInt, 0, nizInt.length - 1);
-//		endTime = System.currentTimeMillis();
-//		ispis(nizInt);
-//		System.out.println("\nVrijeme izvrsenja = " + (endTime - startTime) + "ms\n");
-//		
-//		System.out.println("SEKVENCIJALNI INSERTION SORT SA DOUBLESIMA");
-//		nizDub = sortModel.getRandomDoubles(50);
-//		ispis(nizDub);
-//		startTime = System.currentTimeMillis();
-//		insertionSort.sort(nizDub, 0, nizDub.length - 1);
-//		endTime = System.currentTimeMillis();
-//		ispis(nizDub);
-//		System.out.println("\nVrijeme izvrsenja = " + (endTime - startTime) + "ms\n");
-		
-
 	}
 	
 	public static <T extends Comparable<T>> void testiraj(T[] niz, Sort sorter) {
