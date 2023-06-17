42b6db7f3b0374424666926c7a1b3cdb01e8073e
MilanVlaski <milan.vlaski321@gmail.com>
2023-06-17 08:34:07 +0200

    Uradjeno malo ljepse testiranje u mainu za sekvencijalni bubblesort

--- PRS-program/src/parallel/ParallelSelectionSort.java
+++ PRS-program/src/parallel/ParallelSelectionSort.java
@@ -41,7 +41,7 @@ public class ParallelSelectionSort implements Sort{
 		               for(int j = i + 1; j < desni + 1; j++) {
 		            	   if(niz[j].compareTo(niz[min]) < 0)
 		            		   min = j;
-		               }
+		               }				
 		               
 						T pom = niz[min];
 						niz[min] = niz[i];
